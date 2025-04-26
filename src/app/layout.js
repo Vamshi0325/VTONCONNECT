3407912eb415f938a479196d8bef657f2cf9c8ab
VAMSHI VADE. <vamshiv@STRING.METAVERSE>
2025-04-26 13:42:38 +0530

    card add

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,5 @@
 "use client";
+import Script from "next/script";
 import "./globals.css";
 
 import { TonConnectUIProvider } from "@tonconnect/ui-react";
@@ -10,6 +11,17 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body cz-shortcut-listen="true">
+        <Script
+          src="https://telegram.org/js/telegram-web-app.js"
+          strategy="afterInteractive"
+          onLoad={() => {
+            if (window.Telegram && window.Telegram.WebApp) {
+              const telegram = window.Telegram.WebApp;
+              telegram.ready();
+              telegram.expand();
+            }
+          }}
+        />
         <TonConnectUIProvider manifestUrl={tonConnectOptions.manifestUrl}>
           {children}
         </TonConnectUIProvider>
